0e32fe5456931954e34e938f9dfa7dc3cc6dd206
zhangatle <319126583@qq.com>
2022-09-06 08:40:23 +0800

    Update xiaomi.py

--- function/xiaomi_sports/xiaomi.py
+++ function/xiaomi_sports/xiaomi.py
@@ -76,8 +76,8 @@ def main(user, passwd, step):
         exit(0)
         # return "用户名或密码填写有误！"
     if step == '':
-        print("已设置为随机步数（1829-2356）")
-        step = str(random.randint(1829, 2356))
+        print("已设置为随机步数（19520-23569）")
+        step = str(random.randint(19520, 23569))
     login_token = 0
     login_token, userid = login(user, password)
     if login_token == 0:
